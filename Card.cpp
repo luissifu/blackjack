58750d5df1b38a6d302dddfc94bac07da12079bb
luissifu@laptop <luis.e.sifuentes@gmail.com>
2014-08-24 16:57:10 -0500

    added gl

--- Card.cpp
+++ Card.cpp
@@ -48,9 +48,8 @@ void Card::muestra()
 }
 
 
-void Card::drawBackground(int xMin, int xMax, int yMin, int yMax)
-{
-	glColor3ub(255,255,255);
+void Card::drawBackground(int xMin, int xMax, int yMin, int yMax) {
+	//glColor3ub(255,255,255);
 	glBegin(GL_POLYGON);
 		glVertex2d( xMin,yMin);
 		glVertex2d( xMax,yMin );
@@ -58,7 +57,28 @@ void Card::drawBackground(int xMin, int xMax, int yMin, int yMax)
 		glVertex2d( xMin,yMax);
 	glEnd();
 }
-void Card::draw(int x, int y)
-{
+
+void drawText(int x, int y, std::string text, void* font, int r, int g, int b) {
+	glColor3ub(r, g, b);
+	glRasterPos2i(x, y);
+
+	for (string::iterator i = text.begin(); i != text.end(); ++i)
+	{
+		char c = *i;
+		glutBitmapCharacter(font, c);
+	}
+}
+
+void Card::draw(int x, int y) {
+	glColor3ub(255,255,255);
 	drawBackground(x, x + 50, y, y + 100);
+	std::string text = "";
+	text += v;
+	int r = 0;
+
+	if (s == 'H' || s == 'D')
+		r = 255;
+
+	drawText(x+5, y+20, text, GLUT_BITMAP_HELVETICA_18, r,0,0);
+	drawBackground(x + 10, x + 40, y + 50, y + 80);
 }
